d21c95a27209c3700379c0400ce1ecca7ce4b638
NIKITA-esc <dalalnikita9033@gmail.com>
2024-06-11 12:50:36 +0530

    code added

---
+++ calculator.py
@@ -0,0 +1,40 @@
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+operation = input("Enter the operatio +, -, *, /, //, %, &, or : ")
+
+
+def calculator(num1, num2, operation):
+    if operation == "+":
+      return(num1 + num2)
+    elif operation == "-":
+       return(num1 - num2)
+    elif operation == "*":
+       return(num1 * num2) 
+    elif operation == "/":
+       if num2 == 0:
+        return "invalid input"
+       else:
+        return(num1 / num2) 
+    elif operation == "//":
+      if num2!=0:
+        return(num1 // num2)
+      else:
+        return("invalid input")
+    elif operation == "%":
+      if num2!=0:
+        return(num1 % num2)
+      else:
+        return("invalid input")
+    elif operation == "&":
+      return (num1 & num2)
+    elif operation == "or":
+      return (num1 or num2)
+    else:
+      return "invalid operation"
+    
+result = calculator(num1, num2, operation)
+print(result, "is the result")
+    
+     
+     
+ 
