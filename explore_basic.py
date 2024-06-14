6a410b75ee5969ebe84a02e9e4c755ae06d61d06
NIKITA-esc <dalalnikita9033@gmail.com>
2024-06-14 19:23:05 +0530

    modified

--- explore_basic.py
+++ explore_basic.py
@@ -1,5 +1,5 @@
+# basic operation in python
 str1 = "Python is a case sensitive  language"
-# basic operations of python
 print(len(str1))
 print(str1[9:])  # string slicing
 print(str1.replace("a case sensitive","object oriennted"))
@@ -21,8 +21,33 @@ except ValueError:
 print(str1.replace(" ","")) # remove whitespace
 str2 = "    Python is a good   language to start your programming career     "
 print(str2.strip()) # remove leading and trailing whitespace
-print(" ".join(str2.split())) # remove extra space from string
+print(" ".join(str2.split())) # remove extra whitespace from string
 
+# variables
+name = "nishu"
+sid = 24379
+dept = "civil"
+print(f"Hey {name} here! \n My SID is {sid} \n My DEPARTMENT is {dept} ")
+
+# bitwise operators
+a = 40 # 0b101000
+b = 43 # 0b101011
+print(a & b) # a and b
+print (a | b) # a or b
+print(a ^ b) # XOR between two integers
+print(a>>2 , b>>2) # right shift of a and  by 2
+print(a<<2 , b<<2) # left shift of a and b by 2
+
+# input statement
+name = str(input ("Enter your full name here : "))
+try:
+    rank = int(input ("Enter your rank of JEE MAINS 2024: "))
+    if rank < 40000:
+        print("you got a seat at PEC!!" )
+    else:
+        print("Sorry, you can't grab a seat this year. But don't be sad , it is not the end of life. Give yourself one more chance.")
+except ValueError:
+    print(f"INVALID INPUT!! check {name}, again")
 
 
 
