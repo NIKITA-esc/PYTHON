14e3c7b1ba58ace066c29d48645e7e914ef36630
NIKITA-esc <dalalnikita9033@gmail.com>
2024-06-16 15:49:08 +0530

    modified

--- explore_basic.py
+++ explore_basic.py
@@ -63,16 +63,83 @@ def flip_bits(a,b): # bits needed to convert a to b
     return bin(c).count('1')
 print(flip_bits(2 , 5))
 
-# list methods
-list = ['cady', '12', 'medical', '98']
-print(list.append('mary')) # returns none but modifies the list
-print(list.remove("mary")) # returns none but modifies the list
-#print(list.clear())        # returns none but modifies the list
-print(list.pop())
-print(list.insert(1,"john"))
-print(list.insert(2, "mary"))
-print(len(list))
-print(list)
+# list methods #mutable
+list1 = ['cady', '12', 'medical', '98'] # all data types allowed
+print(list1.append('mary')) # returns none but modifies the list
+print(list1.remove("mary")) # returns none but modifies the list
+#print(list1.clear())       # returns none but modifies the list
+print(list1.pop())
+print(list1.insert(1,"john"))
+print(list1.insert(2, "mary"))
+print(len(list1))
+print(list1)
+
+# tuple methods #immutable
+t = (1, 2, {"name" : "cady"} , [4,5,6], (4,5,6),2) # different data types allowed
+print(len(t))
+print(t[2])
+print(t.index(2,2))  # (value,start,stop)
+for i in t:
+    print(i)
+
+# add items in tuple
+t1 = ({"surname" : "mario"}, )  # byconcatenation
+add = t + t1
+print(add)
+
+t2 = t1 # by slicing
+add1 = t[:3] + t2 + t[3:]
+print(add1)
+
+t3 = list(t) # by converting to list
+t3.insert(6,2)
+print(tuple(t3))
+
+# dictionary methods (key,value pair) , mutable
+dict = {"a" : 2, "b" : 3, "c" : 4}
+print(dict.get("a"))
+print(dict.items())
+print(dict.values())
+print(dict.keys())
+print(dict.update({"a" : 3, "d" : 4}))
+print(dict)
+
+# largest of three numbers
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = int(input("Enter third number: "))
+if a==b==c:
+    print("All the entered numbers has equal values")
+
+elif  a==b:
+    print("first number and second number are equal")
+elif a==c:
+    print("first number and third number are equal")
+elif c==b:
+    print("third number and second number are equal")
+elif a>b and a>c:
+    print(f"{a} is largest number")
+elif c>b and c>a:
+    print(f"{c} is largest number")
+elif b>a and b>c:
+    print(f"{b} is largest number")
+else:
+    print("Enter the correct numbers")
+
+# Using the max function
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = int(input("Enter third number: "))
+
+def maximum(a,b,c):
+    return max(a,b,c)
+
+largest = maximum(a,b,c)
+print("the largest number is ", largest)
+
+
+
+
 
 
 
